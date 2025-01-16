2195325085804d8665176697532fbf50fe4cba84
vrb-tech <vrb-tech@MacBook-Air-admin.local>
2025-01-16 12:24:27 +0200

    auth remastered

--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,15 +1,19 @@
-import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Matches, MinLength } from 'class-validator';
 
 export class loginDto {
-  @IsString()
-  @IsNotEmpty()
+  @IsEmail({}, { message: 'Email must be a valid email address' })
+  @IsNotEmpty({ message: 'Email is required' })
   email: string;
 
-  @IsNotEmpty()
-  @MinLength(8)
+  @IsNotEmpty({ message: 'Password is required' })
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @Matches(/[A-Z]/, { message: 'Password must contain at least one uppercase letter' })
+  @Matches(/[a-z]/, { message: 'Password must contain at least one lowercase letter' })
+  @Matches(/\d/, { message: 'Password must contain at least one number' })
   password: string;
 
-  @IsNotEmpty()
-  @IsString()
-  type: 'user' | 'organisation';
+  @IsNotEmpty({ message: 'Type is required' })
+  @IsString({ message: 'Type must be a string' })
+  @Matches(/^(user|organisation)$/, { message: 'Type must be either "user" or "organisation"' })
+  type: 'user' | 'individual' | 'organisation';
 }
--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -1,70 +1,68 @@
-import { IsBoolean, IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
 import { UserRole } from 'src/types/UserRole';
 
 export class BaseAuthDto {
   @IsBoolean({
-    message: 'The "isDoyles" field must be a boolean value.',
+    message: 'The isDoyles field must be a boolean value.',
   })
   @IsNotEmpty({
-    message: 'The "isDoyles" field cannot be empty.',
+    message: 'The isDoyles field cannot be empty.',
   })
   isDoyles: boolean;
 
-  @IsEmail(
-    {},
-    {
-      message: 'The "email" field must be a valid email address.',
-    },
-  )
-  @IsNotEmpty({
-    message: 'The "email" field cannot be empty.',
-  })
+  @IsEmail({}, { message: 'Email must be a valid email address' })
+  @IsNotEmpty({ message: 'Email is required' })
   email: string;
 
-  @IsString({
-    message: 'The "password" field must be a string.',
-  })
-  @IsNotEmpty({
-    message: 'The "password" field cannot be empty.',
-  })
-  @MinLength(6, {
-    message: 'The "password" field must be at least 6 characters long.',
-  })
+  @IsNotEmpty({ message: 'Password is required' })
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @Matches(/[A-Z]/, { message: 'Password must contain at least one uppercase letter' })
+  @Matches(/[a-z]/, { message: 'Password must contain at least one lowercase letter' })
+  @Matches(/\d/, { message: 'Password must contain at least one number' })
   password: string;
 
+  @IsOptional()
   @IsString({
-    message: 'The "role" field must be a string.',
+    message: 'The role field must be a string.',
   })
   @IsNotEmpty({
-    message: 'The "role" field cannot be empty.',
+    message: 'The role field cannot be empty.',
   })
   role: UserRole;
 }
 
 export class OrganisationRegisterDto extends BaseAuthDto {
   @IsString({
-    message: 'The "name" field must be a string.',
+    message: 'The name field must be a string.',
   })
   @IsNotEmpty({
-    message: 'The "name" field cannot be empty.',
+    message: 'The name field cannot be empty.',
   })
   name: string;
 }
 
 export class UserRegisterDto extends BaseAuthDto {
   @IsString({
-    message: 'The "firstName" field must be a string.',
+    message: 'The firstName field must be a string.',
   })
   @IsNotEmpty({
-    message: 'The "firstName" field cannot be empty.',
+    message: 'The firstName field cannot be empty.',
   })
   firstName: string;
 
   @IsString({
-    message: 'The "lastName" field must be a string.',
+    message: 'The lastName field must be a string.',
   })
   @IsNotEmpty({
-    message: 'The "lastName" field cannot be empty.',
+    message: 'The lastName field cannot be empty.',
   })
   lastName: string;
 }
