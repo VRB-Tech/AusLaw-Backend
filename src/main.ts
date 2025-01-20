16123701e8d1b9ef21d5170c349ce736c088fd5c
vrb-tech <vrb-tech@MacBook-Air-admin.local>
2025-01-20 21:13:43 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import { Sequelize } from 'sequelize-typescript';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    rawBody: true,
+  });
+
   const configService = app.get(ConfigService);
   const port = configService.get<string>('PORT');
 
