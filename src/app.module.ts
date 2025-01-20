c65eeb5c896d84bad86a4bb9a10d7bdbd543e067
vrb-tech <vrb-tech@MacBook-Air-admin.local>
2025-01-20 17:40:51 +0200

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,7 +23,6 @@ import { PostsModule } from './modules/posts/posts.module';
 import { User } from './modules/users/users.model';
 import { UsersModule } from './modules/users/users.module';
 import { NylasModule } from './nylas/nylas.module';
-import { PaymentModule } from './payment/payment.module';
 
 @Module({
   imports: [
@@ -85,7 +84,6 @@ import { PaymentModule } from './payment/payment.module';
     NylasModule,
     WebSocketModule,
     OrganisationsModule,
-    PaymentModule,
   ],
 })
 export class AppModule {}
