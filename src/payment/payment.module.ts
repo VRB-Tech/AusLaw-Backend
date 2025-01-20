bcc990e8a4df31d2a0209e05d8996738c44ef205
vrb-tech <vrb-tech@MacBook-Air-admin.local>
2025-01-20 16:59:24 +0200

    payment fixes

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from 'src/modules/users/users.module';
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
 
 @Module({
+  imports: [UsersModule],
   providers: [PaymentService],
   controllers: [PaymentController],
 })
