fc1ff186d426aaee9de460d5052603eb5b829edd
vrb-tech <vrb-tech@MacBook-Air-admin.local>
2025-01-22 16:51:58 +0200

    subscriptionType removed

--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -37,11 +37,6 @@ export class BaseAuthDto {
     message: 'The role field cannot be empty.',
   })
   role: UserRole;
-
-  @IsString({
-    message: 'The subscriptionType field must be a string.',
-  })
-  subscriptionType: string;
 }
 
 export class OrganisationRegisterDto extends BaseAuthDto {
